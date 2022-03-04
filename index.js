46b04b6ba3ab7a4066b4d39e1811685d9a52b02b
emrgncr <egencer20@ku.edu.tr>
2022-03-04 21:13:23 +0300

    some changes

--- index.js
+++ index.js
@@ -13,16 +13,24 @@ const get_random_line = () => {
     return get_line(line)
 }
 
+/**
+ * Returns a line from the bee movie
+ * @param {Number} min_l minimum number of words
+ * @param {Number} max_l maximum number of words
+ * @param {Number} max_tries maximum iterations until give up
+ * @returns {String} a line from the bee movie
+ */
 const get_random_line_with_word_count = (min_l,max_l,max_tries) => {
     if(min_l == undefined) min_l = 1;
     if(max_l == undefined) max_l = Number.MAX_SAFE_INTEGER;
-    if(max_tries == undefined) max_tries = 400;
+    if(max_tries == undefined) max_tries = 10;
     while(max_tries --> 0){
         const line = get_random_line();
-        const words = line.match('/\w/g') + 1;
+        const words = line.split(' ').length;
+        console.log(words);
         if(words >= min_l && words <= max_l) return line;
     }
-    return get_line(2);
+    return get_line(4);
 
 }
 
